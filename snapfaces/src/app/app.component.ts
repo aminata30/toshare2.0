f63279f6f470546fa5df6f723f10b9a07a46fc8b
aminata30 <aminata.tamanate@done.fr>
2024-05-10 15:42:55 +0200

    observable test

--- snapfaces/src/app/app.component.ts
+++ snapfaces/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
 
 
 @Component({
@@ -8,7 +9,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit{
   ngOnInit(){
+    const interval$ = interval(1000);
     
+    interval$.subscribe(value => console.log(value))
+    setTimeout(() =>interval$.subscribe(value => console.log(value)), 3000); 
   }
 }
 
